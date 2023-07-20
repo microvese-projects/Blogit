c2e508f33adc4e2af814fc8499427c88e738e0fb
Benson <njugunab655@gmail.com>
2023-07-20 11:50:52 +0300

    Update posts counter on user

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
+
+  after_create :update_user_posts_counter
+
+  private
+
+  def update_user_posts_counter
+    author.increment(:posts_counter)
+  end
 end
