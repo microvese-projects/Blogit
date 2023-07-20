ef0f2767c58f47c385145bb8c47b92eeb38dc210
Benson <njugunab655@gmail.com>
2023-07-20 11:51:48 +0300

    Get recent comments on post

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,11 @@ class Post < ApplicationRecord
 
   after_create :update_user_posts_counter
 
+  def recent_comments
+    @post_comments = self.comments.order(created_at: :desc).limit(5)
+    @post_comments
+  end
+
   private
 
   def update_user_posts_counter
