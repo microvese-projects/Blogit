f745f4f060a9db374754deb61117d83d6d2823d0
Benson <njugunab655@gmail.com>
2023-07-20 19:38:16 +0300

    Initialize counters before validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,13 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
 
+  before_validation :initialize_counters
+
   after_create :update_user_posts_counter
 
   validates :title, presence: true, length: { in: 3..250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
@@ -17,5 +19,10 @@ class Post < ApplicationRecord
 
   def update_user_posts_counter
     author.increment!(:posts_counter)
+  end
+
+  def initialize_counters
+    self.comments_counter ||= 0
+    self.likes_counter ||= 0
   end
 end
