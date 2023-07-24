0a4cf31c9bb96210467bc3afb79a054fbc5f55f2
Benson <njugunab655@gmail.com>
2023-07-24 13:33:38 +0300

    Add profile route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
   get '/', to: 'users#index'
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#profile'
 end
