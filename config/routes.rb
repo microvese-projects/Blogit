bba42c3999e2c4fd60dfc7d475693a4c97f1296d
Benson <njugunab655@gmail.com>
2023-07-24 13:51:52 +0300

    Rename param

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#index"
-  get '/', to: 'users#index'
+  # get '/', to: 'users#index'
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
-  get '/users/:id/posts', to: 'posts#index'
+  get '/users/:user_id/posts', to: 'posts#index'
 end
