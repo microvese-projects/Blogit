e8122e71f725546cfb94c54d5c51c097b7b28f96
Benson <njugunab655@gmail.com>
2023-07-25 12:48:57 +0300

    Make tests readable

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,20 +2,38 @@ require 'rails_helper'
 
 RSpec.describe '/posts', type: :request do
   describe 'GET /index' do
-    it 'gets the index page' do
+    before(:each) do
       get '/users/:user_id/posts'
+    end
+
+    it 'Should succesfully get all posts' do
       expect(response.status).to eq(200)
-      expect(response.content_type).to match(a_string_including('text/html'))
+    end
+
+    it 'Should succesfully render a list of posts' do
       expect(response.body).to match(a_string_including('This page lists all posts of a certain user'))
+    end
+
+    it 'Should render Html' do
+      expect(response.content_type).to match(a_string_including('text/html'))
     end
   end
 
   describe 'GET /show' do
-    it 'gets a specific post with id' do
+    before(:each) do
       get '/users/:user_id/posts/:id'
+    end
+
+    it 'gets a specific post with id' do
       expect(response.status).to eq(200)
-      expect(response.content_type).to match(a_string_including('text/html'))
+    end
+
+    it 'Should succesfully render details of a post' do
       expect(response.body).to match(a_string_including('This page shows details of a specific post'))
+    end
+
+    it 'Should render Html' do
+      expect(response.content_type).to match(a_string_including('text/html'))
     end
   end
 end
