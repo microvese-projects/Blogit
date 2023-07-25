7644166ed84938c7c9b5c1331943416e5b7c417b
Benson <njugunab655@gmail.com>
2023-07-25 12:39:38 +0300

    Add request specs for users controller

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe '/users', type: :request do
+  describe 'GET /index' do
+    before(:each) do
+      get '/users'
+    end
+    it 'Should succesfully get a list of all users' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'Should list all users' do
+      expect(response.body).to match(a_string_including('This page lists all users'))
+    end
+  end
+
+  describe 'GET /show' do
+    before(:each) do
+      get '/users/:id'
+    end
+
+    it 'Should succesfully fetch a user\'s profile' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'Should display the user\s data' do
+      expect(response.body).to match(a_string_including('renders the profile of a specific user with a given id'))
+    end
+  end
+end
