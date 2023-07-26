608d9db622933f502c75aa5c0443eb676365e1ea
Benson <njugunab655@gmail.com>
2023-07-26 15:04:45 +0300

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#index"
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    get 'posts', on: :member, to: 'posts#index', as: :posts
+  end
   get '/users/:user_id/posts', to: 'posts#index'
   get '/users/:user_id/posts/:id', to: 'posts#show'
 end
