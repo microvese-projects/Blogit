9a6992135368b58fdd15eac244f509531823cea8
Benson <njugunab655@gmail.com>
2023-07-26 13:13:19 +0300

    Add resources for users controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#index"
-  # get '/', to: 'users#index'
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
+  resources :users, only: [:index, :show]
   get '/users/:user_id/posts', to: 'posts#index'
   get '/users/:user_id/posts/:id', to: 'posts#show'
 end
