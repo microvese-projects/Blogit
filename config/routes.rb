d6802ef374b1d4ca30b46444aae1571a5ce68af4
Benson <njugunab655@gmail.com>
2023-07-27 12:04:17 +0300

    Remove redundant route

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,4 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
-  get '/users/:user_id/posts/:id', to: 'posts#show'
 end
