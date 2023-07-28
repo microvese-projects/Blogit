1b97e9e6b16f29bb651b36767920cac550320934
Benson <njugunab655@gmail.com>
2023-07-28 09:53:07 +0300

    Add comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    unless !current_user
+      @post = Post.find(params[:post_id])
+      @comment = Comment.new(text: values[:comment], author_id: params[:user_id], post_id: params[:post_id])
+      
+      if @comment.save
+        redirect_to user_post_path(params[:user_id], params[:post_id])
+      else
+        render :new, alert: 'Error: Could not add comment.'
+      end
+    end
+  end
+
+  private
+
+  def values
+    params.require(:comment).permit(:comment, :user_id, :post_id)
+  end
+end
