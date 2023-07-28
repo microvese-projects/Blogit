7fe9d28430429a0195ed2895d9309bec665c40ab
Benson <njugunab655@gmail.com>
2023-07-28 11:20:23 +0300

    Fix linter error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,15 +4,15 @@ class CommentsController < ApplicationController
   end
 
   def create
-    unless !current_user
-      @post = Post.find(params[:post_id])
-      @comment = Comment.new(text: values[:comment], author_id: params[:user_id], post_id: params[:post_id])
-      
-      if @comment.save
-        redirect_to user_post_path(params[:user_id], params[:post_id])
-      else
-        render :new, alert: 'Error: Could not add comment.'
-      end
+    return unless current_user
+
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(text: values[:comment], author_id: params[:user_id], post_id: params[:post_id])
+
+    if @comment.save
+      redirect_to user_post_path(params[:user_id], params[:post_id])
+    else
+      render :new, alert: 'Error: Could not add comment.'
     end
   end
 
