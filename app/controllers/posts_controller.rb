3b48e5f9dfd88451edf045e1ea119952213b2627
Benson <njugunab655@gmail.com>
2023-07-31 16:53:12 +0300

    Fix n+1 problems

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts
+    @posts = Post.all.includes(:author).where(users: {id: params[:user_id]})
   end
 
   def show
