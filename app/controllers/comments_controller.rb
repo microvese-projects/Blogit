4d6f6d7efc655afa22a2de7590149b545ee666c0
Benson <njugunab655@gmail.com>
2023-08-04 15:34:31 +0300

    Add post id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,7 +9,7 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = Comment.new(comment_params)
     @comment.author = current_user
-    @comment.post = @post
+    @comment.post_id = params[:post_id]
 
     if @comment.save
       respond_to do |format|
