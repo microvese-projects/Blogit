af1fdc25aa18e1cd31fec102cbef081d5d668d47
Benson <njugunab655@gmail.com>
2023-08-04 13:59:45 +0300

    Fix rubocop errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render :json => @posts }
+      format.json { render json: @posts }
     end
   end
 
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render :json => @comments }
+      format.json { render json: @comments }
     end
   end
 
